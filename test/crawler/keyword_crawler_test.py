13108ca7e3eda309856d7d7a9dde1881b9be19b5
taehong.kim <public.devhong@gmail.com>
2020-04-12 20:21:41 +0900

    Add unit test of `keyword_crawler.py`

---
+++ test/crawler/keyword_crawler_test.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+import unittest
+
+
+class KeywordCrawlerUnitTest(unittest.TestCase):
+
+    @classmethod
+    def setUp(cls):
+        cls.seed_url = "https://www.naver.com"
+
+    def test(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
